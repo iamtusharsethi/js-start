52f6d58c3ba3e69a5095e7d61f609a5cd8555956
iamtusharsethi <104458838+iamtusharsethi@users.noreply.github.com>
2024-02-07 11:47:35 +0000

    container  config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144455
+let accountEmail = "Tushar@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// const can not be allowed to change by an interpreter 
+console.log([accountEmail,accountId,accountPassword,accountCity])
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
